9e951872a3617ab744dc8a31881904f015a35205
rorubyy <roruby118@gmail.com>
2023-03-09 16:21:59 +0800

    2023/03/09 leetcode142

---
+++ medium/leetcode142.cpp
@@ -0,0 +1,50 @@
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+ListNode *detectCycle(ListNode *head)
+{
+    ListNode *fast = head, *slow = head;
+    while (fast && fast->next)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+        if (fast == slow)
+        {
+            break;
+        }
+    }
+    if (!fast || !fast->next)
+        return NULL;
+    fast = head;
+    while (fast != slow)
+    {
+        fast = fast->next;
+        slow = slow->next;
+    }
+    return fast;
+}
+int main()
+{
+    // initialize a cycle list
+    vector<int> values = {3, 2, 0, -4};
+    ListNode *head = new ListNode(values[0]);
+    ListNode *cur = head;
+    for (int i = 1; i < values.size(); i++)
+    {
+        cur->next = new ListNode(values[i]);
+        cur = cur->next;
+    }
+    cur->next = head->next;
+    detectCycle(head);
+    return 0;
+}
