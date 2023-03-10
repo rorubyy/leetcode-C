7201fdf29413e0af3df6a13e09ce19af64609eb2
rorubyy <roruby118@gmail.com>
2023-03-10 13:16:40 +0800

    2023/03/10 random

---
+++ medium/leetcode382.cpp
@@ -0,0 +1,61 @@
+
+#include <iostream>
+#include <vector>
+#include <random>
+
+using namespace std;
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+private:
+    ListNode *head;
+
+public:
+    Solution(ListNode *head)
+    {
+        this->head = head;
+    }
+    int getRandom()
+    {
+        int count = 0, result = 0;
+        ListNode *cur = head;
+        while (cur)
+        {
+            count++;
+            if (rand() % count == 0)
+            {
+                result = cur->val;
+            }
+            cur = cur->next;
+        }
+        return result;
+    }
+};
+
+int main()
+{
+    vector<int> values = {1, 2, 3};
+    ListNode *head = new ListNode(values[0]);
+    ListNode *cur = head;
+    for (int i = 1; i < values.size(); i++)
+    {
+        cur->next = new ListNode(values[i]);
+        cur = cur->next;
+    }
+
+    Solution *obj = new Solution(head);
+    vector<int> randomList(10, 0);
+    for (int i = 0; i < 10; i++)
+    {
+        randomList[i] = obj->getRandom();
+    }
+    return 0;
+}
