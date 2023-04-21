eac3ca41db2685a2b0ab0f9306fc5146617762ed
rorubyy <roruby118@gmail.com>
2023-04-21 16:07:35 +0800

    dp update

--- leetcode/leetcode0879.cpp
+++ leetcode/leetcode0879.cpp
@@ -3,29 +3,26 @@
 using namespace std;
 int profitableSchemes(int n, int minProfit, vector<int> &group, vector<int> &profit)
 {
-    int crimeLen = group.size();
-    vector<vector<vector<int>>> dp(crimeLen + 1, vector<vector<int>>(n + 1, vector<int>(minProfit + 1)));
+    int C = group.size(), MOD = 1e9 + 7;
+    vector<vector<vector<int>>> dp(C + 1, vector<vector<int>>(n + 1, vector<int>(minProfit + 1)));
     dp[0][0][0] = 1;
-    int MOD = 1e9 + 7;
-
-    for (int i = 1; i <= crimeLen; i++)
+    for (int i = 1; i <= C; i++)
     {
         int g = group[i - 1], p = profit[i - 1];
         for (int j = 0; j <= n; j++)
         {
             for (int k = 0; k <= minProfit; k++)
             {
-                dp[i][j][k] = dp[i - 1][j][k] + (j >= g ? dp[i - 1][j - g][max(0, k - p)] : 0);
+                dp[i][j][k] = (dp[i - 1][j][k] + (j >= g ? dp[i - 1][j - g][max(0, k - p)] : 0)) % MOD;
             }
         }
     }
     int ans = 0;
-    for (int j = 0; j <= n; j++)
+    for (int i = 0; i <= n; i++)
     {
-        ans = (ans + dp[crimeLen][j][minProfit]) % MOD;
+        ans = (ans + dp[C][i][minProfit]) % MOD;
     }
-
-    return 1;
+    return ans;
 }
 int main()
 {
